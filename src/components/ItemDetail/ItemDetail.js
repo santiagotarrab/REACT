e78e2468e770874e6674e338cdbdbfa36d3dba05
santiagotarrab <santiagotarrab@gmail.com>
2022-09-09 13:11:33 -0300

    0909

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,15 +1,35 @@
 import getFetch from "../../helper/helper"
 import {useState, useEffect} from 'react'
 import { productos } from "../../helper/helper"
-export const ItemDetail = ({id})=>{
+export const ItemDetail = ({id, pasarValor})=>{
+
+    const [contador,setContador]=useState(0)
+    const sumar = ()=>{setContador(contador+1)}
+    const restar = ()=>{
+        if(contador>=1){setContador(contador-1)}}
 
 
-    console.log()
+    const funcion=(ev)=>{
+        ev.preventDefault()
+        pasarValor(contador)        
+        
+
+    }   
+
     return(
+
 
         <>
        <h1>{productos.filter((prod)=>prod.id==id)[0].name}</h1>
+       <div>
         
+        <img src={`../img/${productos.filter((prod)=>prod.id==id)[0].img}`} alt="" />
+       </div>
+       <button onClick={restar}>-</button> 
+       <button onClick={sumar}>+</button> 
+       <button onClick={funcion}>PASAR VALOR</button> 
+ 
+        <div>{contador}</div>
         </>
     )
 }
--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -4,9 +4,12 @@ import { useParams } from "react-router-dom"
 
 export function ItemDetailContainer(){
   const {id} = useParams()
-  
+   
+  const pasarValor = (contador)=> {
+    alert(contador)}
 
     return(
-    <ItemDetail id={id}/>
+    <ItemDetail id={id} pasarValor={pasarValor}/>
+    
   )  
 }
